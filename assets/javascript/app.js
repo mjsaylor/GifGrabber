eb0de5d9934efc9a77fec7841d8ed353a870738b
Maggie Jo Saylor <saylor.mj@gmail.com>
2019-04-16 20:23:15 -0500

    created functions to render buttons & gifs

---
+++ assets/javascript/app.js
@@ -0,0 +1,63 @@
+
+var heroes = ["Thor", "Captain America", "Hulk", "Black Widow", "Hawkeye", "Iron Man", "Spiderman"]
+
+
+function renderButtons() {
+    $("#render-buttons").empty();
+
+    for (var i = 0; i < heroes.length; i++) {
+        var a = $("<button>");
+        a.addClass("hero-button");
+        a.attr("data-hero", heroes[i]);
+        a.text(heroes[i]);
+        $("#render-buttons").append(a);
+    }
+}
+
+$("#add-hero").on("click", function (event) {
+    event.preventDefault();
+    var hero = $("#hero-input").val().trim();
+    if (heroes.includes(hero)) {
+        console.log("Already Added")
+        return
+    } else {
+        heroes.push(hero);
+    }
+    console.log(heroes);
+    renderButtons();
+});
+
+
+function renderGifs() {
+
+    var hero = $(this).attr("data-hero");
+    console.log(this)
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + hero + "&api_key=SWdoMuBNH9GrMPaYWBF4V2TfSNYgGvlK&limit=10";
+
+    $.get(queryURL)
+        .then(function (response) {
+            console.log(response)
+            var results = response.data;
+
+            for (var i = 0; i < results.length; i++) {
+                var gifDiv = $("<div>");
+
+                var rating = results[i].rating;
+
+                var p = $("<p>").text("Rating: " + rating);
+
+                var heroImage = $("<img>");
+                heroImage.attr("src", results[i].images.fixed_height.url);
+
+                gifDiv.prepend(p);
+                gifDiv.prepend(heroImage);
+
+                $("#render-gifs").prepend(gifDiv);
+            }
+        });
+
+};
+
+$(document).on("click", ".hero-button", renderGifs);
+
+renderButtons();
